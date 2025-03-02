ad52b208346c56247bda9b3b69253ff1b47ea819
Yusup <yusup@lsgrep.com>
2025-03-02 15:40:32 +0800

    cleanup

--- packages/i18n/index.ts
+++ packages/i18n/index.ts
@@ -1,5 +1,5 @@
 import { t as t_dev_or_prod } from './lib/i18n';
-import type { t as t_dev } from './lib/i18n-dev';
+import { t as t_dev } from './lib/i18n-dev';
 import {
   languageCodeToName,
   languageCodeToNativeName,
@@ -7,5 +7,5 @@ import {
   getLanguageNameFromCode,
 } from './lib/languageMap';
 
-export const t = t_dev_or_prod as unknown as typeof t_dev;
+export const t = t_dev;
 export { languageCodeToName, languageCodeToNativeName, availableLanguages, getLanguageNameFromCode };
